3d644404c1f98179b2422a5e3936a163811e31b7
Taylor Zewen <tayzewen@gmail.com>
2023-01-28 18:30:59 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,57 @@
+
+// Event listener for Enter key
+document.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        testUserNum()
+    }
+});
+
+let message = ""
+let messageEl = document.getElementById("messageEl")
+let randomNumber = ""
+let guessesEl = document.getElementById("guessesEl")
+let guess = 0;
+let maxGuess = 5;
+let pointsEl = document.getElementById("pointsEl")
+let points = 0;
+
+function renderGame() {
+    randomNumber = Math.floor(Math.random() * 10) +1;
+    console.log(randomNumber)
+    messageEl.textContent = ""
+    guessesEl.textContent = ""
+    document.getElementById("userNum").value = "";
+    guess = 0
+}
+
+renderGame()
+
+function testUserNum() {
+    let userNum = parseInt(document.getElementById("userNum").value);
+    console.log(userNum)
+
+    if (userNum != randomNumber && guess === (maxGuess-1)) {
+        guess++;
+        message = "Sorry, you're out of guesses :( The number was " + randomNumber
+    } else if (userNum < randomNumber) {
+        guess++;
+        message = "Not quite...try a larger number..."
+        console.log("Not quite...try a larger number...")
+    } else if (userNum > randomNumber) {
+        guess++;
+        message = "No...try a smaller number..."
+        console.log("No...try a smaller number...")
+    } else if (userNum === randomNumber) {
+        guess++;
+        message = "YES! You guessed correctly in " + guess + " guesses. The number was " + randomNumber
+        console.log("YES! You guessed correctly. The number was " + randomNumber);
+        points = points + (maxGuess - guess);
+        pointsEl.textContent = "Points: " + points;
+    } else {
+        message = "Error"
+    }
+
+    messageEl.textContent = message;
+    guessesEl.textContent = "Guesses remaining: " + (maxGuess - guess);
+    document.getElementById("userNum").value ="";
+}
